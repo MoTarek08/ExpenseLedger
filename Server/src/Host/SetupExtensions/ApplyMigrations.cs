1ceb4026f7f7aa8763c647fe7f235b1b8a9e4916
Mohamed Tarek <mohamedcoder22@gmail.com>
2026-08-02 10:46:12 +0300

    ApplyMigrationsExtenstion

---
+++ Server/src/Host/SetupExtensions/ApplyMigrations.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Database.AppDbContextNamespace;
+using Microsoft.EntityFrameworkCore;
+
+namespace Host.SetupExtensions
+{
+    public static class Migrations
+    {
+        public async static Task<WebApplication> ApplyMigrations(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                await db.Database.MigrateAsync();
+                return app;
+            }
+        }
+    }
+}
